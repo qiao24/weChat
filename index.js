43b9f798a80906a39fc77a65340e30d045d68420
qiaoli <757127196@qq.com>
2016-12-13 14:47:13 +0800

    上传

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.jia').on('click',function(){
+        $('.si,.sidianyi').toggleClass('chu');
+    })
+
+
+   var anniu = $('.baliu ul li');
+    var yi = $('.ooo');
+
+    anniu.on('click',function(){
+       var nn = $(this).index();
+        for(i=0;i<yi.length;i++){
+            yi.eq(i).addClass('mei');
+        }
+        yi.eq(nn).removeClass('mei');
+
+    })
+
+
+});
